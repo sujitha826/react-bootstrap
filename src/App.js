79198d6e4df62b074b3079b93c8394b1d12300ea
Sujitha K <sujitha.k@marketsimplified.com>
2022-10-25 13:54:10 +0530

    react-bootstrap-learning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Container, Navbar } from "react-bootstrap";
+import Counter from "./components/Counter";
+import Register from "./components/Register";
+import UserList from "./components/UserList";
 
-function App() {
+let App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" expand="sm" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">React Bootstrap</Navbar.Brand>
+        </Container>
+      </Navbar>
+      {/*<Counter/>*/}
+      {/*<Register/>*/}
+      <UserList />
+    </>
   );
 }
 
